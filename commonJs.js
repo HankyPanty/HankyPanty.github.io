0aadad91c4e3457a7537289a1b183f6ecd583fae
Pranav Totala <pranavtotala48@gmail.com>
2024-06-27 13:50:50 +0530

    added uniform changes

--- commonJs.js
+++ commonJs.js
@@ -29,11 +29,20 @@ function Home() {
 }
 
 function loadCommonContent() {
-  fetch('https://hankypanty.github.io/commonHtml.html')
+    fetch('https://hankypanty.github.io/commonHtml.html')
         .then(response => response.text())
         .then(data => {
             console.log(data.body);
             document.getElementById('common-content').innerHTML = data;
         })
         .catch(error => console.error('Error loading common content:', error));
+}
+
+            
+function dirtyMode() {
+var bagrounds = document.getElementsByClassName("background-image");
+    for (var i = bagrounds.length - 1; i >= 0; i--) {
+        bagrounds[i].style.backgroundImage = "url(https://lh3.googleusercontent.com/pw/AP1GczN3xARjAdVLbrOaQulqXVxcHm-TTcqxWkZxxRFxikNTCkhzn0EPZxjm3l28oMBusopii7DHDLJZkTFNQ1RvY97gsV995VAsQBRMDahx_4WaDTdErhYaOrETP4MSbKaVRmPTDdolTPbFHKMnbd0ZxkFjKQ=w1539-h970-s-no-gm?authuser=0)";
+        bagrounds[i].style.filter = "blur(5px)";
+    }
 }
